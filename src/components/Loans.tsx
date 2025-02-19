7571e10f79e4d580fb6fd10d391b551bea94913c
Obi ifeanyi <obiifeanyi.fi@gmail.com>
2025-02-19 19:35:01 +0100

    Variable declared but not used problem solved

--- src/components/Loans.tsx
+++ src/components/Loans.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Loans = () => {
   return <div>Loans</div>;
 };
--- src/components/Savings.tsx
+++ src/components/Savings.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Savings = () => {
   return <div>Savings</div>;
 };
